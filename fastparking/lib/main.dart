6f39a99835ae2cac3f0f0d05d37436f62ee68940
FernasGui <guilhermealmeidafernandes@gmail.com>
2023-11-21 01:50:41 +0000

    adcionar ao git

---
+++ fastparking/lib/main.dart
@@ -0,0 +1,47 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'FastParking',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.blue),
+        useMaterial3: true,
+      ),
+      home: const HomeScreenStateful(), // Use o StatefulWidget aqui
+    );
+  }
+}
+
+class HomeScreenStateful extends StatefulWidget {
+  const HomeScreenStateful({super.key});
+
+  @override
+  _HomeScreenState createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreenStateful> {
+  // Adicione variáveis e métodos para gerenciar o estado aqui
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('FastParking'),
+      ),
+      body: Center(
+        child: Text(
+          'Bem-vindo ao FastParking',
+          style: Theme.of(context).textTheme.headline4,
+        ),
+      ),
+    );
+  }
+}
