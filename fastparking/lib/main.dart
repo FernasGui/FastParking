fc3990c1dc44e440447a0b82423fba925a3ab0d2
FernasGui <guilhermealmeidafernandes@gmail.com>
2023-11-21 18:01:10 +0000

    ja funcionar

--- fastparking/lib/main.dart
+++ fastparking/lib/main.dart
@@ -11,6 +11,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'FastParking',
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.blue),
         useMaterial3: true,
