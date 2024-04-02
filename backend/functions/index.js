18ecb8ef09875aaa11e7bd5f98bdcb1b82399c4d
FernasGui <guilhermealmeidafernandes@gmail.com>
2024-04-02 15:36:42 +0100

    mapeamento

--- backend/functions/index.js
+++ backend/functions/index.js
@@ -1,25 +1,32 @@
-// const functions = require("firebase-functions");
 const admin = require("firebase-admin");
 const serviceAccount = require("./serviceAccount.json");
-
-/**
- * Inicializa o Firebase Admin SDK se ainda não estiver inicializado.
- */
-function setupAdminSDK() {
-  if (admin.apps.length === 0) {
-    admin.initializeApp({
-      credential: admin.credential.cert(serviceAccount),
-    });
-  }
+const functions = require("firebase-functions");
+// Inicializa o Firebase Admin SDK se ainda não estiver inicializado.
+if (admin.apps.length === 0) {
+  admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount),
+  });
 }
 
-// Importar a função de registro de auth.js.
+/** apagar???
+ * Inicializa o Firebase Admin SDK se ainda não estiver inicializado.
+ */
+// function setupAdminSDK() {
+//   if (admin.apps.length === 0) {
+//     admin.initializeApp({
+//       credential: admin.credential.cert(serviceAccount),
+//     });
+//   }
+// }
+
+// Importa as funções definidas em outros arquivos
 const {registerUser} = require("./auth");
 const {loginUser} = require("./log");
+const {adicionarMatricula, registrarAtualizarEstacionamento, carregarSaldo} = require("./mapeamento");
 
-// Exportar a função setupAdminSDK juntamente com as outras funções
-exports.setupAdminSDK = setupAdminSDK;
-exports.registerUser = registerUser;
-exports.loginUser = loginUser;
-
-// Agora, você pode chamar setupAdminSDK em qualquer lugar que importe este arquivo
+// Exporta as funções do Cloud Functions diretamente
+exports.registerUser = functions.https.onCall(registerUser);
+exports.loginUser = functions.https.onCall(loginUser);
+exports.adicionarMatricula = functions.https.onCall(adicionarMatricula);
+exports.registrarAtualizarEstacionamento = functions.https.onCall(registrarAtualizarEstacionamento);
+exports.carregarSaldo = functions.https.onCall(carregarSaldo);
